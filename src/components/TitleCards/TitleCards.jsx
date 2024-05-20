59998b9c661818f1c2188c899f912f85a5ee2c4f
Shubhang Gupta <guptashubhang2000@gmail.com>
2024-05-20 13:57:15 +0530

    clone update

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
+import { Link } from "react-router-dom";
 
 const TitleCards = ({ title, category }) => {
   const [apiData, setApiData] = useState([]);
@@ -23,7 +24,9 @@ const TitleCards = ({ title, category }) => {
 
   useEffect(() => {
     fetch(
-      `https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`,
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
       options
     )
       .then((response) => response.json())
@@ -39,10 +42,13 @@ const TitleCards = ({ title, category }) => {
       <div className="card-list" ref={cardsRef}>
         {apiData.map((card, index) => {
           return (
-            <div className="card" key={index}>
-              <img src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path} alt="" />
+            <Link to={`/player/${card.id}`} className="card" key={index}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
+                alt=""
+              />
               <p>{card.original_title}</p>
-            </div>
+            </Link>
           );
         })}
       </div>
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,19 +1,62 @@
-import React from 'react'
-import './Player.css'
-import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import React, { useEffect, useState } from "react";
+import "./Player.css";
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Player = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: "",
+  });
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNzVjM2E4YTI0ZDBiMDE1MWJjNzEwZDM2YzM2MmQ3MyIsInN1YiI6IjY2NDcwYjY2NGVkMDQwYjM5MjJkZjM1MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vh5g7WCektRwzF6MrEXdduFVnus_h9p1ptetXOcmu1w",
+    },
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setApiData(response.results[0]))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <div className='player'>
-      <img src={back_arrow_icon} alt='' />
-      <iframe width='90%' height='90%' src='https://www.youtube.com/embed/YQQD67N5pi0' title='trailer' frameBorder='0' allowFullScreen></iframe>
+    <div className="player">
+      <img
+        src={back_arrow_icon}
+        alt=""
+        onClick={() => {
+          navigate(-2);
+        }}
+      />
+      <iframe
+        width="90%"
+        height="90%"
+        src={`https://www.youtube.com/embed/${apiData.key}`}
+        title="trailer"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
       <div className="player-info">
-        <p>Published Date</p>
-        <p>Name</p>
-        <p>Type</p>
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Player
+export default Player;
